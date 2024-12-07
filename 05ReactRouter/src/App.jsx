06d1b15a4ac9e10cac252f4fe11dc1874dc8dbfd
Yash <yashrana21032006@gmail.com>
2024-12-07 12:39:05 +0530

    .

--- 05ReactRouter/src/App.jsx
+++ 05ReactRouter/src/App.jsx
@@ -1,25 +1,9 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
 
 import "./App.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Home from "./components/Home";
-import About from "./components/About";
-import Contact from "./components/Contact";
 
 function App() {
-  return (
-    <>
-      <Router>
-        <Header />
-        <Home />
-        <About />
-        <Contact />
-        <Footer />
-      </Router>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
